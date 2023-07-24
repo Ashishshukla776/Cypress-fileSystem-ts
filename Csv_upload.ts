5a00738ef402415793d7a4fe59324462836a2b67
Ashish Shukla <100287606+Ashishshukla776@users.noreply.github.com>
2023-07-24 12:02:09 +0530

    Update Csv_upload.ts

--- Csv_upload.ts
+++ Csv_upload.ts
@@ -2,29 +2,17 @@ import routes from "../../support/route";
 
 describe(`Test case for csv_upload`, () => {
   it(`Success test case for csv_upload`, () => {
-    let allmodules = [
-      "body",
-      "category",
-      "color",
-      "customer_store",
-      "customers",
-      "design",
-      "division",
-      "factor",
-      "group"
-    ];
-    allmodules.forEach((value) => {
-      cy.fixture(value + ".csv").then((binary) => {
+      cy.fixture("color.csv").then((binary) => {    // csv file will come from Fixture folder of your poject
         const filedata = Cypress.Blob.binaryStringToBlob(binary, "text/csv");
         const formData = new FormData();
         formData.set("csv", filedata);
-        formData.set("module", value);
+        formData.set("module", color);
         cy.postFormData(
-          routes.csv_import,
-          formData,
-          Cypress.env("companyToken")
+          routes.csv_import, //url
+          formData,  //body
+          Cypress.env("auth-token") // Auth-token
         ).then((body) => {
-          cy.log(JSON.stringify(value))
+          cy.log(JSON.stringify(body))
           expect(body.data).has.property("statusCode", 200);
           expect(body.data).has.property("success", true);
           expect(body.data).has.property("error", null);
@@ -33,5 +21,5 @@ describe(`Test case for csv_upload`, () => {
           expect(body.data.result.data).has.property("id");
         })
       });
-    });
   });
+})
